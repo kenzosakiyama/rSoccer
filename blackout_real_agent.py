5801b4dfa80ccbe46a6869d775868ae61daad896
joao guilherme melo <jgocm@cin.ufpe.br>
2023-03-03 12:25:45 -0300

    parametric plotter working version

--- blackout_real_agent.py
+++ blackout_real_agent.py
@@ -31,9 +31,10 @@ def parse_particle_filter_data(measurements, global_movement, robot_w, data):
     movement = [dx, dy, dtheta]
     return movement, goal, field_points
 
-def process_plot(xs, y1, y2, y3):
+def process_plot(xs, ys):
+    n_plots = 3
     fig = plt.figure(figsize=(6.75,9.3))
-    ax1, ax2, ax3 = fig.subplots(nrows=3, ncols=1)
+    ax = fig.subplots(nrows=n_plots, ncols=1)
     fig.subplots_adjust(left=0.1, 
                         bottom=0.1, 
                         right=0.9, 
@@ -41,37 +42,31 @@ def process_plot(xs, y1, y2, y3):
                         wspace=0.4,
                         hspace=0.4)
     list_x = []
-    list_y1 = []
-    list_y2 = []
-    list_y3 = []
+    list_ys = []
+    ax_titles = ["RMSE",
+                 "Similarity from Particles Filter Median Particle",
+                 "Non-normalized Weights Sum"]
+    
+    for i in range(0, n_plots):
+        list_ys.append([])
 
     def update(*args, **kwargs):
         list_x = args[1]
-        list_y1 = args[2]
-        list_y2 = args[3]
-        list_y3 = args[4]
+        list_ys = args[2]
         _x = xs.get()
-        _y1 = y1.get()
-        _y2 = y2.get()
-        _y3 = y3.get()
+        _y = ys.get()
         list_x.append(_x)
-        list_y1.append(_y1)
-        list_y2.append(_y2)
-        list_y3.append(_y3)
+        for i in range(0, n_plots):
+            list_ys[i].append(_y[i])
+            ax[i].clear()
+            ax[i].plot(list_x, list_ys[i])
+            ax[i].set_title(ax_titles[i])
+
         # Draw x and y lists
-        ax1.clear()
-        ax1.plot(list_x, list_y1)
-        ax2.clear()
-        ax2.plot(list_x, list_y2)
-        ax3.clear()
-        ax3.plot(list_x, list_y3)
-        ax1.set_title("RMSE")
-        ax2.set_title("Similarity from Particles Filter Median Particle")
-        ax3.set_title("Non-normalized Weights Sum")
 #        list_x = list_x[-20:]
 #        list_y = list_y[-20:]
 
-    _ = animation.FuncAnimation(fig, update, fargs=(list_x, list_y1, list_y2, list_y3), interval=1)
+    _ = animation.FuncAnimation(fig, update, fargs=(list_x, list_ys,), interval=1)
     plt.show()
 
 if __name__ == "__main__":
@@ -80,7 +75,7 @@ if __name__ == "__main__":
     import time
     cwd = os.getcwd()
 
-    n_particles = 150
+    n_particles = 50
     vertical_lines_nr = 1
 
     # LOAD REAL ODOMETRY DATA
@@ -99,7 +94,7 @@ if __name__ == "__main__":
 
     # SET INITIAL ROBOT POSITION AND SEED
     initial_position = position[0]
-    seed_radius = 10
+    seed_radius = 1
     seed_x, seed_y, seed_theta = initial_position
     initial_position[2] = np.degrees(initial_position[2])
 
@@ -130,10 +125,9 @@ if __name__ == "__main__":
     counter = 0
 
     xs = mp.Queue(maxsize=len(position))
-    y1 = mp.Queue(maxsize=len(position))
-    y2 = mp.Queue(maxsize=len(position))
-    y3 = mp.Queue(maxsize=len(position))
-    side_process = mp.Process(target=process_plot, args=(xs, y1, y2, y3))
+    ys = mp.Queue(maxsize=3*len(position))
+
+    side_process = mp.Process(target=process_plot, args=(xs, ys))
     side_process.start()
 
     while env.steps<len(position)-1:
@@ -153,14 +147,13 @@ if __name__ == "__main__":
         env.update_particles(robot_tracker.particles, odometry_tracking, particles_filter_tracking, time_steps[env.steps])
         RMSE = np.sqrt((particles_filter_tracking[0]-position[env.steps][0])**2 + (particles_filter_tracking[1]-position[env.steps][1])**2)
         env.render()
-        if counter<100:
+        if counter<0:
             env.update_step(0)
             counter += 1
 
+        _ys = [RMSE, robot_tracker.average_particle_weight, robot_tracker.prior_sum_weights/robot_tracker.n_particles]
         xs.put(env.steps)
-        y1.put(RMSE)
-        y2.put(robot_tracker.average_particle_weight)
-        y3.put(robot_tracker.prior_sum_weights/robot_tracker.n_particles)
+        ys.put(_ys)
 
     side_process.terminate()
     side_process.join()
